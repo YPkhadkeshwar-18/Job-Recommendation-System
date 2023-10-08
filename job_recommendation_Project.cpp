e736f7270007fa38b30c81e635ad549a1f12c2c3
Tanu-KR <141993463+Tanu-KR@users.noreply.github.com>
2023-10-08 12:22:02 +0530

    Update job_recommendation_Project.cpp

--- job_recommendation_Project.cpp
+++ job_recommendation_Project.cpp
@@ -1,13 +1,10 @@
 #include <bits/stdc++.h>
 #define ll long long 
 using namespace std;
-
-
 bool comp(pair<int, pair<string, int>>a, pair<int, pair<string, int>>b)
 {
     return a.first > b.first;
 }
-
 void solve()
 {
     map<string, vector<pair<int, string>>>job;
@@ -25,26 +22,21 @@ void solve()
     job["Fashion_designer"] = {{15, "Both_passion_and_money"}, {25, "Perfect_planner"}, {35, "Love_your_job"}, {45, "Something_creative"}};
     job["Artist"] = {{15, "Work_from_home"}, {25, "Imagination_beyond_bound"}, {35, "Something_creative"}, {45, "Artistic"}};
     job["Philosopher"] = {{15, "Calm"}, {25, "Confidence"}, {35, "Positive_attitude"}, {45, "Good_communication"}};
-    job["Government_servant"] = {{15, ""}, {25, "To_achieve_big_success"}, {35, "9 to 5 job"}, {45, "Study"}};
-
-
-    
-    job["Dancer"] = {{15, "Analytical_thinking"}, {25, "Problem_solving_ability"}, {35, "Research_skill"}, {45, "Flexible"}};
-    job["Beautician"] = {{15, "Analytical_thinking"}, {25, "Problem_solving_ability"}, {35, "Research_skill"}, {45, "Curiosity"}};
-    job["Actor"] = {{15, "Analytical_thinking"}, {25, "Problem_solving_ability"}, {35, "Research_skill"}, {45, "Curiosity"}};
-    job["Manager"] = {{15, "Analytical_thinking"}, {25, "Problem_solving_ability"}, {35, "Research_skill"}, {45, "Curiosity"}};
-    job["Architect"] = {{15, "Analytical_thinking"}, {25, "Problem_solving_ability"}, {35, "Research_skill"}, {45, "Curiosity"}};
-    job["Astronaut"] = {{15, "Analytical_thinking"}, {25, "Problem_solving_ability"}, {35, "Research_skill"}, {45, "Curiosity"}}; 
-    job["Judge"] = {{15, "Analytical_thinking"}, {25, "Problem_solving_ability"}, {35, "Research_skill"}, {45, "Curiosity"}};
-    job["Businessman"] = {{15, "Analytical_thinking"}, {25, "Problem_solving_ability"}, {35, "Research_skill"}, {45, "Curiosity"}};
-    job["Chartered_accountant"] = {{15, "Analytical_thinking"}, {25, "Problem_solving_ability"}, {35, "Research_skill"}, {45, "Curiosity"}};
-    job["Sportsman"] = {{15, "Analytical_thinking"}, {25, "Problem_solving_ability"}, {35, "Research_skill"}, {45, "Curiosity"}};
-    
+    job["Government_servant"] = {{15, "Sustainable"}, {25, "To_achieve_big_success"}, {35, "9 to 5 job"}, {45, "Study"}};
+    job["Dancer"] = {{15, "Team_work"}, {25, "Boldness"}, {35, "Enthausiastic"}, {45, "Flexible"}};
+    job["Beautician"] = {{15, ""}, {25, "Love_your_job"}, {35, "Can work overtime if pays well"}, {45, "Something_creative"}};
+    job["Actor"] = {{15, "Both_passion_and_money"}, {25, "Artistic"}, {35, "Communication_skill"}, {45, "Boldness"}};
+    job["Manager"] = {{15, "Smart_work"}, {25, "Striclty_professional"}, {35, "Good_communication"}, {45, "Dealing_with_people"}};
+    job["Architect"] = {{15, "Problem_solving_ability"}, {25, "Innovative"}, {35, "Analytical_thinking"}, {45, "Perfect_planner"}};
+    job["Astronaut"] = {{15, "Analytical_thinking"}, {25, "Research_skill"}, {35, "Adapting_surrounding"}, {45, "Study"}}; 
+    job["Sportsman"] = {{15, "Team_work"}, {25, "Hard_work"}, {35, "Leadership"}, {45, "Enthausiastic"}};
+    job["Judge"] = {{15, "9 to 5 job"}, {25, "Problem_solving_ability"}, {35, "Calm"}, {45, "Decision_making_skills"}};
+    job["Businessman"] = {{15, "Hard_work"}, {25, "Problem_solving_ability"}, {35, "Dealing_with_people"}, {45, "To_achieve_big_success"}};
+    job["Chartered_accountant"] = {{15, "Analytical_thinking"}, {25, ""}, {35, "Problem_solving_ability"}, {45, "Study"}};
     int n; cin >> n;
     string skill[n];
     for(int i = 0;i < n;i++) cin >> skill[i];
     vector<pair<int, pair<string, int>>>skillset_count;
-
     int ct = 0, score = 0;
     for(int i = 0;i < n;i++){
         string check = skill[i];
@@ -58,7 +50,6 @@ void solve()
       }
     }
     skillset_count.push_back({score, {"Doctor", ct}});
-
     ct = 0, score = 0;
     for(int i = 0;i < n;i++){
         string check = skill[i];
@@ -71,7 +62,6 @@ void solve()
       }
     }
    skillset_count.push_back({score, {"Poet", ct}});
-
     ct = 0, score = 0;
     for(int i = 0;i < n;i++){
         string check = skill[i];
@@ -84,7 +74,6 @@ void solve()
       }
     }
     skillset_count.push_back({score, {"Professor", ct}});
-  
     ct = 0, score = 0;
     for(int i = 0;i < n;i++){
         string check = skill[i];
@@ -97,7 +86,6 @@ void solve()
       }
     }
     skillset_count.push_back({score, {"Soldier", ct}});
-  
     ct = 0, score = 0;
     for(int i = 0;i < n;i++){
         string check = skill[i];
@@ -110,7 +98,6 @@ void solve()
       }
     }
     skillset_count.push_back({score, {"Engineer", ct}});
-  
     ct = 0, score = 0;
     for(int i = 0;i < n;i++){
         string check = skill[i];
@@ -123,7 +110,6 @@ void solve()
       }
     }
     skillset_count.push_back({score, {"Political", ct}});
-  
     ct = 0, score = 0;
     for(int i = 0;i < n;i++){
         string check = skill[i];
@@ -136,7 +122,6 @@ void solve()
       }
     }
     skillset_count.push_back({score, {"Lawyer", ct}});
-  
     ct = 0, score = 0;
     for(int i = 0;i < n;i++){
         string check = skill[i];
@@ -149,7 +134,6 @@ void solve()
       }
     }
     skillset_count.push_back({score, {"Chef", ct}});
-  
     ct = 0, score = 0;
     for(int i = 0;i < n;i++){
         string check = skill[i];
@@ -162,7 +146,6 @@ void solve()
       }
     }
     skillset_count.push_back({score, {"Author", ct}});
-  
     ct = 0, score = 0;
     for(int i = 0;i < n;i++){
         string check = skill[i];
@@ -175,13 +158,11 @@ void solve()
       }
     }
     skillset_count.push_back({score, {"Scientist", ct}});
-
     sort(skillset_count.begin(), skillset_count.end(), comp);
     for(auto it: skillset_count){
         cout << it.first << " " << it.second.first << " " << it.second.second << "\n";
     }
 }
-
 int main(){   
    solve();
 }
